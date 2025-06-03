410e937e2930e2c79a9ebd0d8a9f8d3808270774
smartitsoloutions <prasads128312@gmail.com>
2025-06-03 16:44:59 +0530

    this is my 2nd day commit of this project

--- src/test/java/io/buildpacks/example/sample/SampleApplicationTests.java
+++ src/test/java/io/buildpacks/example/sample/SampleApplicationTests.java
@@ -1,15 +1,16 @@
 package io.buildpacks.example.sample;
 
-import org.junit.Test;
+import org.junit.Test1;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+commit code into java
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SampleApplicationTests {
 
-	@Test
+	@Test1
 	public void contextLoads() {
 	}
 
